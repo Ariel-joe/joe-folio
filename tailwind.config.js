0a65a8987c2d30d9e7d8bde31ff7726ed9d877ea
Ariel-Joe <arieljoe@gmail.com>
2024-11-12 23:08:03 +0300

    mont font added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'mont-font': ['Montserrat', 'Arial', 'sans-serif'],
+      },
+    },
   },
   plugins: [require('tailwindcss-motion')],
 }
